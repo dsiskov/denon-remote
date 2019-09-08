1d329c9d39d53c01b23dd22d9faf7083a125172a
dsiskov <siskov@nielsen.com>
2019-09-08 13:57:24 +0200

    minor additions

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -5,10 +5,8 @@ import axios from 'axios'
 import Slider from 'react-input-slider';
 import * as routes from '../utils/routes'
 import * as commands from '../utils/commands'
-
-// icons
+import * as inputTypes from '../utils/inputs'
 import { ReactComponent as PowerIcon } from '../assets/icons/power.svg'
-
 import "./homePage.css";
 
 export default class HomePage extends Component {
@@ -29,6 +27,7 @@ export default class HomePage extends Component {
 	}
 
 	togglePower = () => {
+		console.log('power')
 		const commandParameter = this.state.avrSettings.power === 'STANDBY' ? 'ON' : 'OFF';
 		console.log(commandParameter);
 		this.executeCommand(commands.TOGGLE_POWER, commandParameter)
@@ -36,23 +35,33 @@ export default class HomePage extends Component {
 
 	// todo: setup debounce
 	setVolume = (newVolume) => {
+		console.log(newVolume)
 		// commands.SET_VOLUME
 	}
 
 	async getAvrSettings() {
 		let response = await axios.get(routes.SETTINGS_ROUTE)
 		this.setState({ avrSettings: response.data.result })
+		console.log(response.data.result)
+	}
+
+	executeCommandWrapper(args) {
+		this.executeCommand(args.command, args.commandParameter);
 	}
 
 
 	async executeCommand(command, commandParameter) {
+		console.log(`executing: ${command} with paramter: ${commandParameter}`)
 		await axios.post(routes.EXECUTE_ROUTE, { command, commandParameter })
 		this.getAvrSettings()
 	}
 
 	render() {
-		const icons = [
-			<PowerIcon />, <PowerIcon />, <PowerIcon />, <PowerIcon />, <PowerIcon />
+		const allCommands = [
+			<CommandButton name="Internet Radio" action={() => this.executeCommandWrapper({ command: commands.SET_INPUT, commandParameter: inputTypes.INTERNET_RADIO })} />,
+			<CommandButton name="TV Audio" action={() => this.executeCommandWrapper({ command: commands.SET_INPUT, commandParameter: inputTypes.TV_AUDIO })} />,
+			<CommandButton name="DVD" action={() => this.executeCommandWrapper({ command: commands.SET_INPUT, commandParameter: inputTypes.DVD })
+			} />
 		]
 		return (
 			<div className="container-fluid">
@@ -63,7 +72,7 @@ export default class HomePage extends Component {
 				}}>
 					{/* top row */}
 					<Row>
-						<Col xs={{ size: 1, offset: 11 }} style={{ marginTop: "10px" }}>
+						<Col xs={{ size: 1, offset: 11 }} style={{ marginTop: "40px" }}>
 							<button className="unstyled-button" onClick={this.togglePower}>
 								<PowerIcon />
 							</button>
@@ -72,14 +81,19 @@ export default class HomePage extends Component {
 
 					<Row>
 						<Col xs={{ size: 3 }} sm={{ size: 3 }}>
-							{icons.map(x => x)}
+							{allCommands[0]}
 						</Col>
-
+						<Col xs={{ size: 3 }} sm={{ size: 3 }}>
+							{allCommands[1]}
+						</Col>
+						<Col xs={{ size: 3 }} sm={{ size: 3 }}>
+							{allCommands[2]}
+						</Col>
 					</Row>
 
 					<div style={{ marginTop: "auto" }}>
-						<Row class="wrapper">
-							<Col xs={{ size: 12 }} style={{ marginTop: "10px", }}>
+						<Row className="wrapper">
+							<Col xs={{ size: 12 }} style={{ marginBottom: "40px", }}>
 								<Slider
 									style={{ width: "100%" }}
 									axis="x"
@@ -94,4 +108,12 @@ export default class HomePage extends Component {
 			</div>
 		)
 	}
+}
+
+export function CommandButton(props) {
+	return (
+		<button className="unstyled-button" onClick={props.action}>
+			{props.name}
+		</button>
+	);
 }
--- src/utils/inputs.js
+++ src/utils/inputs.js
@@ -1,3 +1,5 @@
 export const INTERNET_RADIO = `IRADIO`;
+export const DVD = `DVD`
+export const TV_AUDIO = `TV`
 
 
